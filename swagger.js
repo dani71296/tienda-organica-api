5aead164d3ee664908a2f57586387b82d0c61b06
dani71296 <israel.tudela712.96@gmail.com>
2026-07-19 19:57:01 -0600

    change of host

--- swagger.js
+++ swagger.js
@@ -5,12 +5,12 @@ const doc = {
         title: 'Organic Food Store API',
         description: 'API documentation for the Organic Food Store project (Part 1 - CRUD Operations).',
     },
-    host: 'localhost:8080',
-    schemes: ['http', 'https'],
+   
+    host: 'tienda-organica-api.onrender.com',
+    schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./src/routes/index.js'];
-
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
